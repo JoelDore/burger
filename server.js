c072b176e93af8b825c37289260e970e938fdfb4
JES20 <dore.joel.dore@gmail.com>
2021-01-02 17:22:58 -0500

    📁 Setup file/directory structure

---
+++ server.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '.public'));
+
+app.listen(PORT, () => {
+    console.log('Server listening on PORT %s', PORT)
+});
