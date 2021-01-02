b4d2ae872ecc252fce09ac51cfb778ec7626d8b9
JES20 <dore.joel.dore@gmail.com>
2021-01-02 18:17:35 -0500

    🗃️🌱 Database setup

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burgers_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY(id)
+);
---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burgers_name)
+VALUES('Hamburger'),
+    ('Cheeseburger'),
+    ('Veggie Burger'),
+    ('Turkey Burger');
