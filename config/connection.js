61b271ea30514c3b72f336407e37fca6540f56fa
JES20 <dore.joel.dore@gmail.com>
2021-01-08 12:23:13 -0500

    🦈 Update MySQL connection for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 require('dotenv').config()
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: process.env.MYSQL_PORT || 3306,
-    user: 'root',
-    password: process.env.PASSWORD,
-    database: process.env.DB
-});
+let connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: process.env.MYSQL_PORT || 3306,
+        user: 'root',
+        password: process.env.PASSWORD,
+        database: process.env.DB
+    });
+}
 
 connection.connect(err => {
     if (err) throw err;
